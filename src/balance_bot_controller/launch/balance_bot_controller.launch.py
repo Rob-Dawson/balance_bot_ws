e5f2c584b07d7a70f3318377fa7d219a2515379c
Rob-Dawson <robdawson123.rd@gmail.com>
2026-02-11 17:40:07 +0000

    Light formatting and import changes

--- src/balance_bot_controller/launch/balance_bot_controller.launch.py
+++ src/balance_bot_controller/launch/balance_bot_controller.launch.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
-import os
 from launch_ros.actions import Node
-from ament_index_python import get_package_share_directory
-from launch_ros.substitutions import FindPackageShare
 from launch import LaunchDescription
 
 def generate_launch_description():
@@ -18,7 +15,7 @@ def generate_launch_description():
     balance_bot_controller = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["balance_bot_controller", 
+        arguments=["balance_bot_controller",
                    "--controller-manager", 
                    "/controller_manager"]
     )
