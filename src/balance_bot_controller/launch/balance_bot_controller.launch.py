87adaec7b5c352c30ebe6813ed4ca6e19121744e
Rob-Dawson <robdawson123.rd@gmail.com>
2026-02-23 18:44:37 +0000

    Added madgwick filter node, IMU noise and increased the update rate

--- src/balance_bot_controller/launch/balance_bot_controller.launch.py
+++ src/balance_bot_controller/launch/balance_bot_controller.launch.py
@@ -12,6 +12,21 @@ def generate_launch_description():
                    "/controller_manager"],
     )
 
+    madgwick_filter = Node(
+        package="imu_filter_madgwick",
+        executable="imu_filter_madgwick_node",
+        name="madgwick_filter",
+        output="screen",
+        parameters=[{
+            "use_sim_time":True,
+            "use_mag":False,
+            "world_frame":"enu",
+            "publish_tf":True,
+            "gain":0.05,
+            "zeta":0.01,
+        }]
+    )
+
     balance_bot_controller = Node(
         package="controller_manager",
         executable="spawner",
@@ -22,6 +37,7 @@ def generate_launch_description():
     
     ld = LaunchDescription()
     ld.add_action(joint_state_broadcaster)
+    ld.add_action(madgwick_filter)
     ld.add_action(balance_bot_controller)
     
     
