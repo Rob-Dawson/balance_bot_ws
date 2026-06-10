843c604c83fa26bc9aea1ef184dd4b8770f2a790
Rob-Dawson <robdawson123.rd@gmail.com>
2026-06-10 12:13:26 +0100

    Added state change output. Added encoder and motor inits Including some test and debug code for the motors and encoders. This seems to work

--- firmware/BalanceBot_V1/src/main.cpp
+++ firmware/BalanceBot_V1/src/main.cpp
@@ -1,12 +1,58 @@
 #include <Arduino.h>
 #include "IMUStateMachine.hpp"
+#include "Encoder.hpp"
+#include "MotorDriver.hpp"
+
+const char* stateToString(IMUState state)
+{
+    switch (state)
+    {
+        case IMUState::CALIBRATING_GYRO:
+            return "CALIBRATING_GYRO";
+        case IMUState::CALIBRATING_ZERO:
+            return "CALIBRATING_ZERO";
+        case IMUState::RUNNING:
+            return "RUNNING";
+        default:
+            return "UNKNOWN";
+    }
+}
+
 IMUStateMachine imu;
+IMUState previousState = imu.getState();
+
+Encoder encoder;
+MotorDriver motor;
 
 void setup() {
   Serial.begin(9600);
   imu.imuInit();
+  encoder.init();  
+  motor.init();
+}
+
+void onEnterState(IMUState state)
+{
+  Serial.println(stateToString(state));
 }
 
 void loop() {
   imu.update();
+  IMUState state = imu.getState();
+  if (state != previousState)
+  {
+    onEnterState(state);
+    previousState = state;
+  }
+  if (state != IMUState::RUNNING) return; 
+
+  motor.setRightPWM(0);
+  motor.setLeftPWM(0);
+
+  Serial.print("Left Wheel Speed: ");
+  Serial.print(encoder.getSpeedLeft(),2);
+
+  Serial.print("\t\t\tRight Wheel Speed: ");
+  Serial.println(encoder.getSpeedRight(),2);
+
 }
