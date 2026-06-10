fd9eae26e79bc4ebaa9d7f531f5f1b3862e32c96
Rob-Dawson <robdawson123.rd@gmail.com>
2026-06-10 12:05:43 +0100

    Added direction and used to change the sign of the pwm value, 255 Forward, -255 Backward. Also performed a small refactor

--- firmware/BalanceBot_V1/include/Encoder.hpp
+++ firmware/BalanceBot_V1/include/Encoder.hpp
@@ -1,24 +1,35 @@
 #include <Arduino.h>
 
+namespace {
+    volatile int8_t leftDirection = 0;
+    volatile int8_t rightDirection = 0;
 
+    volatile uint32_t lastLhTime = 0;
+    volatile uint32_t lastRhTime = 0;
+    volatile uint32_t elapsedTimeLeft = 0;
+    volatile uint32_t elapsedTimeRight = 0;
+
+    constexpr uint8_t lhEncoderA = 3;
+    constexpr uint8_t lhEncoderB = 13;
+
+    constexpr uint8_t rhEncoderA = 2;
+    constexpr uint8_t rhEncoderB = 8;
+}
 class Encoder
 {
 public:
     void init();
     float getSpeedRight();
     float getSpeedLeft();
+    int8_t getLeftDirection();
+    int8_t getRightDirection();
+
 
 private:
 
-    const float countsPerRev = 1000.0f;
-    float wheelRadPerSec = 0.0;
+    static constexpr float m_countsPerRev = 1000.0f;
+    static constexpr uint32_t m_timeout = 50000; //50 ms
 
-
-    const int lh_encoder_A = 3;
-    const int lh_encoder_B = 13;
-
-    const int rh_encoder_A = 2;
-    const int rh_encoder_B = 8;
-    const unsigned int timeout = 50000; //50 ms
+    static constexpr float m_micro2Sec = 1000000.0f;
 
 };
--- firmware/BalanceBot_V1/src/Encoder.cpp
+++ firmware/BalanceBot_V1/src/Encoder.cpp
@@ -1,60 +1,124 @@
 #include "Encoder.hpp"
 
+// 1 = Forward
+// -1 = Backward
 
-volatile unsigned long lastLhTime = 0;
-volatile unsigned long lastRhTime = 0;
-volatile unsigned long elapsedTimeLeft;
-volatile unsigned long elapsedTimeRight;
+
 static void leftEncoderEvent()
 {
-    unsigned long currentTime = micros();
+    uint32_t currentTime = micros();
     if (lastLhTime == 0)
     {
         lastLhTime = currentTime;
         return;
     }
     elapsedTimeLeft = currentTime - lastLhTime;
-
     lastLhTime = currentTime;
-}
 
+
+    // This function fires only if encoderA changes
+    // 00
+    // 01
+    // 11
+    // 10
+    // 00
+
+    // This function will fire at 
+    // 00 11 00
+    int8_t encoderA = digitalRead(lhEncoderA);
+    int8_t encoderB = digitalRead(lhEncoderB);
+
+    if (encoderA != encoderB)
+    {
+        leftDirection = -1;
+    }
+    else
+    {
+        leftDirection = 1;
+    }
+    
+}
 static void rightEncoderEvent()
 {
-    unsigned long currentTime = micros();
+    uint32_t currentTime = micros();
     if (lastRhTime == 0)
     {
         lastRhTime = currentTime;
         return;
     }
+    
     elapsedTimeRight = currentTime - lastRhTime;
     lastRhTime = currentTime;
+
+    int8_t encoderA = digitalRead(rhEncoderA);
+    int8_t encoderB = digitalRead(rhEncoderB);
+
+    if (encoderA != encoderB)
+    {
+        rightDirection = -1;
+    }
+    else
+    {
+        rightDirection = 1;
+    }
+
 }
 
 float Encoder::getSpeedLeft()
 {
-    if (micros() - lastLhTime > timeout) return 0.0f;
-    if (elapsedTimeLeft == 0) return 0.0f; 
+    uint32_t elapsed;
+    uint32_t lastTime;
 
-    float countsPerSecond = 1000000/elapsedTimeLeft;
-    wheelRadPerSec = countsPerSecond * (2*PI / countsPerRev);
+    noInterrupts();
+    elapsed = elapsedTimeLeft;
+    lastTime = lastLhTime;
+    interrupts();
+
+    if (micros() - lastTime > m_timeout) return 0.0f;
+    if (elapsed == 0) return 0.0f; 
+    
+    float countsPerSecond = m_micro2Sec/elapsed;
+    float wheelRadPerSec = countsPerSecond * (2*PI / m_countsPerRev);
+    wheelRadPerSec = wheelRadPerSec * Encoder::getLeftDirection();
     return wheelRadPerSec;
 }
 
 float Encoder::getSpeedRight()
 {
-    if (micros() - lastRhTime > timeout) return 0.0f;
-    if (elapsedTimeRight == 0) return 0.0f; 
-    float countsPerSecond = 1000000/elapsedTimeRight;
-    wheelRadPerSec = countsPerSecond * (2*PI / countsPerRev);
+    uint32_t elapsed;
+    uint32_t lastTime;
+    int8_t direction;
+    
+    noInterrupts();
+    elapsed = elapsedTimeRight;
+    lastTime = lastRhTime;
+    interrupts();
+ 
+    if (micros() - lastTime > m_timeout) return 0.0f;
+    if (elapsed == 0) return 0.0f; 
+    float countsPerSecond = m_micro2Sec/elapsed;
+    float wheelRadPerSec = countsPerSecond * (2*PI / m_countsPerRev);
+    
+    wheelRadPerSec * Encoder::getRightDirection();
     return wheelRadPerSec;
+}
+
+int8_t Encoder::getLeftDirection()
+{
+    return leftDirection;
+}
+
+int8_t Encoder::getRightDirection()
+{
+    return rightDirection;
 }
 
 void Encoder::init()
 {
-    pinMode(lh_encoder_A, INPUT);
-    pinMode(lh_encoder_B, INPUT);
-    pinMode(rh_encoder_A, INPUT);
-    pinMode(rh_encoder_B, INPUT);
-    attachInterrupt(digitalPinToInterrupt(lh_encoder_A), leftEncoderEvent, CHANGE);
-    attachInterrupt(digitalPinToInterrupt(rh_encoder_A), rightEncoderEvent, CHANGE);
+    pinMode(lhEncoderA, INPUT);
+    pinMode(lhEncoderB, INPUT);
+    pinMode(rhEncoderA, INPUT);
+    pinMode(rhEncoderB, INPUT);
+    attachInterrupt(digitalPinToInterrupt(lhEncoderA), leftEncoderEvent, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(rhEncoderA), rightEncoderEvent, CHANGE);
 }
