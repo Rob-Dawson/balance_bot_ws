72f542281645128c80599458105b8881b0eb689f
Rob-Dawson <robdawson123.rd@gmail.com>
2026-07-04 19:54:05 +0100

    Included updatePD, updatePI and updatePID. This needs refactoring but okay for now

--- firmware/BalanceBot_V1/include/PIDController.hpp
+++ firmware/BalanceBot_V1/include/PIDController.hpp
@@ -1,38 +1,46 @@
 #include "Arduino.h"
 
 struct OutputLimits {
-  int16_t minOutput;
-  int16_t maxOutput;
+    int16_t minOutput;
+    int16_t maxOutput;
 };
 
 class PIDController {
 public:
-  // float updatePI(float error, unsigned long dt);
-  float updatePD(float error, float measuredRate);
+    float updatePD(float error, float measuredRate);
+    float updatePI(float error, float dt);
+    float updatePID(float error, float measuredRate, float dt);
 
-  void setPD(float Kp, float Kd);
-  void setP(float Kp);
-  void setD(float Kd);
+    void setPD(float Kp, float Kd);
+    void setPI(float Kp, float Ki);
+    void setPID(float Kp, float Ki, float Kd);
+    void setP(float Kp);
+    void setI(float Ki);
+    void setD(float Kd);
 
-  void setOutputLimits(int16_t maxRate, int16_t minRate);
-  OutputLimits getMinMaxOutput() const { return {m_minOutput, m_maxOutput}; };
+    void setOutputLimits(int16_t maxRate, int16_t minRate);
+    OutputLimits getMinMaxOutput() const { return {m_minOutput, m_maxOutput}; };
 
-  float getKp() const { return m_outputKp; };
-  float getKd() const { return m_outputKd; };
-  float getRawOutput() const { return m_controlOutputRaw; };
+    float getKp() const { return m_outputKp; };
+    float getKi() const { return m_outputKi; };
+    float getKd() const { return m_outputKd; };
+
+    float getRawOutput() const { return m_controlOutputRaw; };
 
 private:
-  float m_Kp{};
-  float m_Kd{};
-  float m_measuredRate{}; // Pitch Rate
-  float m_error{};        // error = self.setpoint - self.imu_pitch
+    float m_Kp{};
+    float m_Ki{};
+    float m_Kd{};
+    float m_measuredRate{}; // Pitch Rate
+    float m_error{};        // error = self.setpoint - self.imu_pitch
+    float m_outputKp{};
+    float m_outputKi{};
+    float m_outputKd{};
+    float m_integral{};
 
-  float m_outputKp{};
-  float m_outputKd{};
+    float m_controlOutput{};
+    float m_controlOutputRaw{};
 
-  float m_controlOutput{};
-  float m_controlOutputRaw{};
-
-  int16_t m_maxOutput{};
-  int16_t m_minOutput{};
+    int16_t m_maxOutput{};
+    int16_t m_minOutput{};
 };
--- firmware/BalanceBot_V1/src/PIDController.cpp
+++ firmware/BalanceBot_V1/src/PIDController.cpp
@@ -1,28 +1,92 @@
 #include <PIDController.hpp>
 
 void PIDController::setOutputLimits(int16_t minOutput, int16_t maxOutput) {
-  m_minOutput = minOutput;
-  m_maxOutput = maxOutput;
+    m_minOutput = minOutput;
+    m_maxOutput = maxOutput;
+}
+void PIDController::setPI(float Kp, float Ki) {
+    m_Kp = Kp;
+    m_Kd = 0;
+    m_Ki = 0;
 }
 void PIDController::setPD(float Kp, float Kd) {
-  m_Kp = Kp;
-  m_Kd = Kd;
+    m_Kp = Kp;
+    m_Kd = Kd;
+    m_Ki = 0;
 }
 
 float PIDController::updatePD(float error, float measuredRate) {
-  m_outputKp = m_Kp * error;
-  m_outputKd = m_Kd * measuredRate;
+    m_outputKp = m_Kp * error;
+    m_outputKi = 0;
+    m_outputKd = m_Kd * measuredRate;
+    m_controlOutput = m_outputKp - m_outputKd;
 
-  m_controlOutput = m_outputKp - m_outputKd;
-  m_controlOutput = m_controlOutputRaw;
-  if (m_controlOutput >= m_maxOutput)
-    m_controlOutput = m_maxOutput;
+    if (m_controlOutput >= m_maxOutput)
+        m_controlOutput = m_maxOutput;
 
-  else if (m_controlOutput <= m_minOutput)
-    m_controlOutput = m_minOutput;
+    else if (m_controlOutput <= m_minOutput)
+        m_controlOutput = m_minOutput;
 
-  return m_controlOutput;
+    return m_controlOutput;
+}
+
+float PIDController::updatePI(float error, float dt) {
+    m_outputKp = m_Kp * error;
+    m_outputKi = m_Ki * m_integral;
+    m_outputKd = 0;
+    float rawOutput = m_outputKp + m_outputKi;
+    if (m_Ki != 0) {
+        if (dt > 0.0 && dt < 0.01) {
+            if (rawOutput > m_maxOutput && error < 0) {
+                m_integral += error * dt;
+            } else if (rawOutput < m_minOutput && error > 0) {
+                m_integral += error * dt;
+            } else if ((rawOutput >= m_minOutput && rawOutput < m_maxOutput)) {
+                m_integral += error * dt;
+            }
+        }
+    }
+    m_outputKi = m_Ki * m_integral;
+    m_controlOutput = m_outputKp + m_outputKi;
+    m_controlOutputRaw = m_controlOutput;
+
+    if (m_controlOutput >= m_maxOutput)
+        m_controlOutput = m_maxOutput;
+
+    else if (m_controlOutput <= m_minOutput)
+        m_controlOutput = m_minOutput;
+
+    return m_controlOutput;
+}
+
+float PIDController::updatePID(float error, float measuredRate, float dt) {
+    m_outputKp = m_Kp * error;
+    m_outputKd = m_Kd * measuredRate;
+    m_outputKi = m_Ki * m_integral;
+    float rawOutput = m_outputKp + m_outputKi - m_outputKd;
+    if (m_Ki != 0) {
+        if (dt > 0.0 && dt < 0.01) {
+            if (rawOutput > m_maxOutput && error < 0) {
+                m_integral += error * dt;
+            } else if (rawOutput < m_minOutput && error > 0) {
+                m_integral += error * dt;
+            } else if ((rawOutput >= m_minOutput && rawOutput < m_maxOutput)) {
+                m_integral += error * dt;
+            }
+        }
+    }
+    m_outputKi = m_Ki * m_integral;
+    m_controlOutput = m_outputKp + m_outputKi - m_outputKd;
+    m_controlOutputRaw = m_controlOutput;
+    if (m_controlOutput >= m_maxOutput)
+        m_controlOutput = m_maxOutput;
+
+    else if (m_controlOutput <= m_minOutput)
+        m_controlOutput = m_minOutput;
+
+    return m_controlOutput;
 }
 
 void PIDController::setP(float Kp) { m_Kp = Kp; }
+void PIDController::setI(float Ki) { m_Ki = Ki; }
 void PIDController::setD(float Kd) { m_Kd = Kd; }
